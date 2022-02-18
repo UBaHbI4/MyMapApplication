2edad55a499076904f47adbb40d217debea8f55a
UBaHbI4 <kinoshnik_ivan@mail.ru>
2022-02-19 02:13:41 +0700

    Перевод build.gradle файлов в формат kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+val sdf = SimpleDateFormat("yyyy.M.dd")
+val currentDate: String = sdf.format(Date())
+
+android {
+    compileSdk = Config.COMPILE_SDK
+
+    defaultConfig {
+        applicationId = Config.APPLICATION_ID
+        minSdk = Config.MIN_SDK_VERSION
+        targetSdk = Config.TARGET_SDK
+        versionCode = Config.VERSION_CODE
+        versionName = currentDate
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = Config.java_version
+        targetCompatibility = Config.java_version
+    }
+
+    kotlinOptions {
+        jvmTarget = Config.JVM_TARGET
+    }
+
+    buildFeatures {
+        android.buildFeatures.viewBinding = true
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+
+    implementation(Kotlin.CORE)
+
+    // Design
+    implementation(Design.APPCOMPAT)
+    implementation(Design.MATERIAL)
+    implementation(Design.CONSTRAINT_LAYOUT)
+    implementation(Design.NAVIGATION_FRAGMENT)
+    implementation(Design.NAVIGATION_UI_KTX)
+
+    // LifeCycle
+    implementation(LifeCycle.LIVEDATA_KTX)
+    implementation(LifeCycle.VIEW_MODEL_KTX)
+
+    // Tests
+    testImplementation(Tests.JUNIT)
+    androidTestImplementation(Tests.TEST_EXT_JUNIT)
+    androidTestImplementation(Tests.ESPRESSO)
+}
