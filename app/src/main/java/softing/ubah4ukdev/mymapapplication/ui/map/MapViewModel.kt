6b906f0ce8cd935dca0ff57362335fc8acd66aa6
UBaHbI4 <kinoshnik_ivan@mail.ru>
2022-02-19 01:35:24 +0700

    Создание проекта

---
+++ app/src/main/java/softing/ubah4ukdev/mymapapplication/ui/map/MapViewModel.kt
@@ -0,0 +1,13 @@
+package softing.ubah4ukdev.mymapapplication.ui.map
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MapViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is map Fragment"
+    }
+    val text: LiveData<String> = _text
+}
